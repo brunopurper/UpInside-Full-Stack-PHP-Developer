085c958c8a70263c255ab9a723898fca7e21614c
Bruno Purper Farias <brunoplusultra@outlook.com>
2021-03-14 02:53:15 -0300

    ups

--- fsphp/02-iniciando-do-zero-com-php/02-06-arrays-vetores-e-pilhas/index.php
+++ fsphp/02-iniciando-do-zero-com-php/02-06-arrays-vetores-e-pilhas/index.php
@@ -7,21 +7,64 @@ fullStackPHPClassName("02.06 - Arrays, vetores e pilhas");
  */
 fullStackPHPClassSession("index array", __LINE__);
 
+$arrA = array(1,2,3);
+$arrA = [0,1,2,3];
+var_dump($arrA);
+
+$arrayIndex = [
+    "Brian",
+    "Angus",
+    "Malcolm"
+];
+
+$arrayIndex[]="Cliff";
+$arrayIndex[]="Phill";
+
+var_dump($arrayIndex);
 
 /**
  * [ associative array ] "key" => "value"
  */
 fullStackPHPClassSession("associative array", __LINE__);
 
+$arrayAssoc =[
+
+    "vocal" => "Brian",
+    "solo_guitar" => "Angus",
+    "base_guitar" => "Malcolm",
+    "bass_guitar" => "Cliff"
+];
+
+$arrayAssoc["drums"] = "Phil";
+$arrayAssoc["rock_band"] = "AC/DC";
+
+var_dump($arrayAssoc);
 
 /**
  * [ multidimensional array ] "key" => ["key" => "value"]
  */
 fullStackPHPClassSession("multidimensional array", __LINE__);
 
+$brian = ["Brian", "Mic"];
+$angus = ["name" => "Angus", "instrument" => "Guitar"];
+$instruments = [
+    $brian,
+    $angus
+];
+
+var_dump($instruments);
 
 /**
  * [ array access ] foreach(array as item) || foreach(array as key => value)
  */
 fullStackPHPClassSession("array access", __LINE__);
 
+$acdc = [
+    "vocal" => "Brian",
+    "solo_guitar" => "Angus",
+    "base_guitar" => "Malcolm",
+    "base_guitar" => "Cliff",
+    "drums" => "Phill"
+];
+
+echo "<p>O vocal da banda AC/DC é  {$acdc["vocal"]}, e junto com {$acdc['solo_guitar']} fazem um ótimo show de rock!</p>";

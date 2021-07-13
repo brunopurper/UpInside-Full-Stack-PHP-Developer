3b76911646491161aa64ab700f5e434d290491cf
Bruno Purper Farias <53791374+brunopurper@users.noreply.github.com>
2021-07-13 12:44:16 -0300

    a

---
+++ fsphp/02-iniciando-do-zero-com-php/02-11-trabalhando-com-funcoes/functions.php
@@ -0,0 +1,24 @@
+<?php
+
+function functionName($arg1, $arg2 , $arg3) {
+    $body = [$arg1, $arg2, $arg3];
+    return $body;
+}
+
+function optionArgs($arg1, $arg2 = true, $arg3 = null){
+    $body = [$arg1, $arg2, $arg3];
+    return $body;
+}
+
+function calcImc(){
+  global $peso;
+  global $altura;
+
+  return $peso / ($altura * $altura);
+};
+
+function payTotal ($price){
+    static $total;
+    $total += $price;
+    return"<p>O total a pagar é de R$ ".number_format($total, decimals: "2", decimal_separator: ",", thousands_separator: ".")."</p>";
+}
--- fsphp/02-iniciando-do-zero-com-php/02-11-trabalhando-com-funcoes/index.php
+++ fsphp/02-iniciando-do-zero-com-php/02-11-trabalhando-com-funcoes/index.php
@@ -6,21 +6,35 @@ fullStackPHPClassName("02.11 - Trabalhando com funções");
  * [ functions ] https://php.net/manual/pt_BR/language.functions.php
  */
 fullStackPHPClassSession("functions", __LINE__);
+require __DIR__."/functions.php";
 
+var_dump(functionName(arg1: "Pearl Jam", arg2: "AC/DC", arg3: "Alter Bridge"));
+var_dump(functionName(arg1: "Robson Jam", arg2: "Kaue /DC", arg3: "Gustavo Bridge"));
 
+var_dump(optionArgs(arg1: "Robson"));
+var_dump(optionArgs(arg1: "Robson", arg2: "Kaue"));
+var_dump(optionArgs(arg1: "Robson", arg2: "Kaue", arg3: "Gustavo"));
 /*
  * [ global access ] global $var
  */
 fullStackPHPClassSession("global access", __LINE__);
-
+$peso = 86;
+$altura = 1.83;
+echo "O seu IMC é de: ", calcImc();
 
 /*
  * [ static arguments ] static $var
  */
 fullStackPHPClassSession("static arguments", __LINE__);
 
+$pay = payTotal(price:200);
+$pay = payTotal(price:150);
+$pay = payTotal(price:500);
 
+echo $pay;
 /*
  * [ dinamic arguments ] get_args | num_args
  */
 fullStackPHPClassSession("dinamic arguments", __LINE__);
+
+

900e0cf16a2d9fd639172e7403e5db9eee9a23cb
Bruno Purper Farias <53791374+brunopurper@users.noreply.github.com>
2021-07-15 00:28:05 -0300

    af

--- fsphp/02-iniciando-do-zero-com-php/02-12-valores-constantes/index.php
+++ fsphp/02-iniciando-do-zero-com-php/02-12-valores-constantes/index.php
@@ -1,15 +1,20 @@
 <?php
 
 require __DIR__ . '/../../fullstackphp/fsphp.php';
+
 fullStackPHPClassName("02.12 - Constantes e constantes mágicas");
 /*
  * [ constantes ] https://php.net/manual/pt_BR/language.constants.php
  */
 fullStackPHPClassSession("constantes", __LINE__);
+
 define("COURSE", "Full Stack PHP");
+
 const AUTHOR = "Robson";
+
 $formation = true;
 if ($formation) {
+    
     define("COURSE_TYPE", "Formação");
 };
 /*
